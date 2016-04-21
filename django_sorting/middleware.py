ca8745b46f81a09e555cbe9fcfec5c6de8cae957
Gilang Chandrasa <gilang.chandrasa@gmail.com>
2016-04-21 15:57:36 +0700

    Django 1.9 compatibility
    Replace self.REQUEST

--- django_sorting/middleware.py
+++ django_sorting/middleware.py
@@ -1,15 +1,17 @@
 def get_field(self):
     try:
-        field = self.REQUEST['sort']
+        field = self.GET.get('sort', self.POST.get('sort', ''))
     except (KeyError, ValueError, TypeError):
         field = ''
     return (self.direction == 'desc' and '-' or '') + field
 
+
 def get_direction(self):
     try:
-        return self.REQUEST['dir']
+        return self.GET.get('dir', self.POST.get('dir', ''))
     except (KeyError, ValueError, TypeError):
         return 'desc'
+
 
 class SortingMiddleware(object):
     """
