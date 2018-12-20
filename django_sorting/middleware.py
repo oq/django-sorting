8dfdd6ff658042f9f466867e54a00dd65c64007b
Enggal Mandiri <oqmailbox@yahoo.com>
2018-12-20 21:11:10 +0700

    Django 1.10 compatibility.

--- django_sorting/middleware.py
+++ django_sorting/middleware.py
@@ -14,9 +14,16 @@ def get_direction(self):
 
 
 class SortingMiddleware(object):
+    def __init__(self, get_response):
+    self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
     """
     Inserts a variable representing the field (with direction of sorting)
-    onto the request object if it exists in either **GET** or **POST** 
+    onto the request object if it exists in either **GET** or **POST**
     portions of the request.
     """
     def process_request(self, request):
