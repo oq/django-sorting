92b7b221d26e0087d5c8662e9c106156bc46c7eb
Enggal Mandiri <oqmailbox@yahoo.com>
2018-12-20 21:17:29 +0700

    .

--- django_sorting/middleware.py
+++ django_sorting/middleware.py
@@ -15,7 +15,7 @@ def get_direction(self):
 
 class SortingMiddleware(object):
     def __init__(self, get_response):
-    self.get_response = get_response
+        self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
